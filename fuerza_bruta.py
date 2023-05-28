b94ace6a17cacc80cc6c5764ffbd95169ba9e559
RealSopaper <luis.ugartechea.enp5.804@gmail.com>
2023-05-28 11:34:46 -0600

    practica 10 funcional

---
+++ fuerza_bruta.py
@@ -0,0 +1,28 @@
+from string import ascii_letters, digits
+from itertools import product
+from time import time
+
+caracteres = ascii_letters+ digits
+
+def buscador (con):
+    archivo = open("combinaciones.txt", "w")
+
+    if 3<= len(con) <=4:
+        for i in range(3,5):
+            for comb in product (caracteres, repeat = i):
+                prueba = "".join(comb)
+                archivo.write(prueba + "\n")
+                if prueba == con:
+                    print("Tu contraseña es {}".format(prueba))
+                    archivo.close()
+                    break
+    else:
+        print ("Ingresa una contraseña que contenga de 3 a 4 caracteres")
+
+
+t0 = time()
+con = 'aaaa'
+buscador(con)
+t1 = time()
+print ("Tiempo de ejecución: {}".format(round(t1-t0, 4)))
+
---
+++ greedy.py
@@ -0,0 +1,17 @@
+from time import time
+
+def cambio(cantidad, denominaciones):
+    resultado = []
+    while cantidad > 0:
+        if cantidad >= denominaciones[0]:
+            num = cantidad // denominaciones[0] #el // es para division entera
+            cantidad = cantidad - (num * denominaciones[0])
+            resultado.append([denominaciones[0], num])
+        denominaciones = denominaciones[1:]
+        print (denominaciones)
+    return resultado
+
+t0 = time()
+print (cambio(4893, [500, 200, 100, 50, 20, 5, 1]))
+t1 = time()
+print ("Tiempo de ejecución{}".format(round(t1-t0, 4)))
